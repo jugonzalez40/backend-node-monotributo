e01d8f482aadfa6af1654456eec2034cc452141a
JuanSS96 <jsgonzalez@smartsoftint.com>
2018-02-20 14:11:46 -0500

    Logger adjuntado

--- server/api/test.js
+++ server/api/test.js
@@ -4,7 +4,7 @@ class Test {
 	constructor(dbConnection) {
 		this.db = dbConnection;
 		this.router = express.Router();
-		this.path = '/test'
+		this.path = '/test';
 		//
 		this.router.get(this.path, this.get);
 		this.router.get(`${this.path}/:id`, this.getById);
@@ -17,27 +17,28 @@ class Test {
 	}
 
 	getById(req, res, next) {
-		res.send(`Test by id: ${req.params.id}`);
+		let m = req.params + parseInt(req.params.kj);
+			res.send(`Test by id: ${m}`);
 	}
 
 	get(req, res, next) {
-		res.send('Test get')
+		res.send('Test get');
 	}
 
 	post(req, res, next) {
-		res.send('Test post')
+		res.send('Test post');
 	}
 
 	put(req, res, next) {
-		res.send('Test put')
+		res.send('Test put');
 	}
 
 	patch(req, res, next) {
-		res.send('Test patch')
+		res.send('Test patch');
 	}
 
 	delete(req, res, next) {
-		res.send('Test delete')
+		res.send('Test delete');
 	}
 }
 
--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
 //External imports
 import express from 'express';
 import bodyParser from 'body-parser';
-import Router from './router'
+import morgan from 'morgan';
+import Router from './router';
+import fs from 'fs';
 //Config
-import SERVER_CONFIG from '../config/server.config.json'
+import SERVER_CONFIG from '../config/server.config.json';
 
 class Server {
-	constructor(){
+	constructor() {
 		this.app = express();
-		this.app.use(bodyParser.urlencoded({extended: false }));
-		this.app.use(bodyParser.json())
+		this.app.use(bodyParser.urlencoded({extended: false}));
+		this.app.use(bodyParser.json());
+		this.app.use(morgan('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}));
+		//Console logger
+		this.app.use(morgan('dev'));
+
 		this.router = new Router(this.app);
 		this.start();
-	}	
-
-	start(){
-		this.app.listen(SERVER_CONFIG.port, this.connected);
 	}
 
-	connected (){
+	start() {
+		this.app.listen(SERVER_CONFIG.port, this.connected);
+		this.app.on('error', () => {
+			console.log('Fuck off');
+		});
+	}
+
+	connected() {
 		console.log('connected');
 	}
 }
